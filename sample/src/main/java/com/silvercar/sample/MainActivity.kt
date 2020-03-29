e7d4ae81ce956a72cebfccae16234ebe6a1dddbd
Matthew de Anda <matthew.de.anda@silvercar.com>
2020-03-29 15:49:27 -0500

    Added Unleash and updated the UI to show that it is fetching a feature toggle.
    Before pulling in the source code of Unleash as a module, setting up the
    Sample app to use the library and confirm it is setup correctly and working.
    
    Other changes:
    - Updated Android Studios to 3.6.1
    - Added Coroutines
    - Set to compile for Java 1.8
    - Updated Material colors and Launcher icon from the default ones.

--- sample/src/main/java/com/silvercar/sample/MainActivity.kt
+++ sample/src/main/java/com/silvercar/sample/MainActivity.kt
@@ -1,12 +1,77 @@
 package com.silvercar.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import no.finn.unleash.DefaultUnleash
+import no.finn.unleash.Unleash
+import no.finn.unleash.event.UnleashReady
+import no.finn.unleash.event.UnleashSubscriber
+import no.finn.unleash.repository.FeatureToggleResponse
+import no.finn.unleash.repository.ToggleCollection
+import no.finn.unleash.util.UnleashConfig
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var unleash: Unleash
+    private lateinit var unleashEnabled: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        supportActionBar?.let { setTitle(R.string.app_name) }
+
+        unleashEnabled = findViewById(R.id.unleashEnabled)
+
+        GlobalScope.launch {
+            unleash = initUnleash()
+            val isEnabled = unleash.isEnabled("test-android")
+            unleashEnabled.text = getString(R.string.unleash_test_android_enabled_formatted,
+                isEnabled)
+        }
+    }
+
+    private suspend fun initUnleash(): Unleash {
+        lateinit var unleash: Unleash
+
+        return withContext(Dispatchers.IO) {
+            suspendCoroutine<Unleash> { coroutine ->
+                val config: UnleashConfig = UnleashConfig.builder()
+                    .appName(application.packageName)
+                    .unleashAPI("https://unleash.silvercar.com/api")
+                    .fetchTogglesInterval(TimeUnit.MINUTES.toSeconds(INTERVAL))
+                    .sendMetricsInterval(TimeUnit.MINUTES.toSeconds(INTERVAL))
+                    .subscriber(object : UnleashSubscriber {
+                        override fun onReady(ready: UnleashReady?) {
+                            Timber.d("Unleash is ready")
+                            coroutine.resume(unleash)
+                        }
+
+                        override fun togglesFetched(toggleResponse: FeatureToggleResponse?) {
+                            toggleResponse?.let { Timber.d("Fetch toggles with status: %s", it.status) }
+                        }
+
+                        override fun togglesBackedUp(toggleCollection: ToggleCollection?) {
+                            Timber.d("Backup stored.")
+                        }
+                    })
+                    .build()
+                unleash = DefaultUnleash(config, EnvironmentStrategy())
+            }
+        }
+    }
+
+    private companion object {
+        const val INTERVAL: Long = 60
     }
 }
